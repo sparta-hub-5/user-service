d1f0fa6c6a6d2c6a28ee0447ce0c45fb10fee46f
HeeHo Lim <121926482+hiho1010@users.noreply.github.com>
2025-11-17 10:33:19 +0900

    first commit

---
+++ src/test/java/org/bangbang/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bangbang.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
