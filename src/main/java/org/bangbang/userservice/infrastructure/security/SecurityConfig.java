0c3c23a1e7599aa119575229bf354450c58cf974
HeeHo Lim <121926482+hiho1010@users.noreply.github.com>
2025-11-19 13:02:59 +0900

    [Feat] Add security configuration and update application properties

---
+++ src/main/java/org/bangbang/userservice/infrastructure/security/SecurityConfig.java
@@ -0,0 +1,36 @@
+package org.bangbang.userservice.infrastructure.security;
+
+import org.bangbang.userservice.infrastructure.keycloak.converter.KeycloakClientRoleConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationEntryPoint;
+import org.springframework.security.oauth2.server.resource.web.access.BearerTokenAccessDeniedHandler;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        JwtAuthenticationConverter conv = new JwtAuthenticationConverter();
+        conv.setJwtGrantedAuthoritiesConverter(new KeycloakClientRoleConverter());
+
+        http.csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/profile/**", "/password/**", "/role/**").hasRole("USER")
+                .anyRequest().permitAll())
+            .oauth2Login(AbstractHttpConfigurer::disable)
+            .oauth2ResourceServer(c -> c
+                .jwt(jwt -> jwt.jwtAuthenticationConverter(conv))
+                .authenticationEntryPoint(new BearerTokenAuthenticationEntryPoint())
+                .accessDeniedHandler(new BearerTokenAccessDeniedHandler()));
+
+        return http.build();
+    }
+}
