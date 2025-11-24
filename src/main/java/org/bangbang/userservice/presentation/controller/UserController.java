8d9c71106b27d252535ac970234d8853fa882cc4
HeeHo Lim <121926482+hiho1010@users.noreply.github.com>
2025-11-24 16:33:26 +0900

    [Refactor] gateway 루트 설정을 위해 url 추가

--- src/main/java/org/bangbang/userservice/presentation/controller/UserController.java
+++ src/main/java/org/bangbang/userservice/presentation/controller/UserController.java
@@ -27,11 +27,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/user")
 public class UserController {
 
     private final TokenGenerateService tokenService;
